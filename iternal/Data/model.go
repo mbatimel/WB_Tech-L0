99c63f76f28d3f2c284e079fb08f5d1ec0a8d80c
mangushev-ildar <mangushev.ildar@bk.ru>
2024-04-06 17:32:53 +0300

    make data model in gp

--- iternal/Data/model.go
+++ iternal/Data/model.go
@@ -1,1 +1,56 @@
 package data
+import "Time"
+type Payment struct{
+	Transaction   string `json:"transaction"`
+	Request_id    string `json:"request_id"`
+	Curency       string `json:"curency"`
+	Provider      string `json:"provider"`
+	Amount        int    `json:"amount"`
+	Payment_dt    int 	 `json:"payment_dt"`
+	Banck         string `json:"banck"`
+	Delivery_cost int    `json:"delivery_cost"`
+	Goods_total   int    `json:"goods_total"`
+	Custom_free   int    `json:"custom_free"`
+
+}
+type Items struct{
+	Chtr_id 		int 	`json:"chtr_id"`
+	Track_number 	string 	`json:"track_number"`
+	Price			int 	`json:"price"`
+	Rid 			string  `json:"rid"`
+	Name 			string  `json:"name"`
+	Sale 			int  	`json:"sale"`
+	Size 			int 	`json:"size"`
+	Total_price 	int 	`json:"total_size"`
+	Nm_id 			int 	`json:"nm_id"`
+	Brand 			string  `json:"brand"`
+	Status 			int 	`json:"status"`
+}
+type Delivery struct{
+	Name    string `json:"name"`
+	Phone   string `json:"phone"`
+	Zip     string `json:"zip"`
+	City    string `json:"city"`
+	Address string `json:"address"`
+	Region  string `json:"region"`
+	Email   string `json:"email"`
+}
+type Order struct{
+	Id                int
+	OrderUid          string    `json:"order_uid"`
+	TrackNumber       string    `json:"track_number"`
+	Entry             string    `json:"entry"`
+	Delivery          Delivery  `json:"delivery"`
+	Payment           Payment   `json:"payment"`
+	Items             []Items    `json:"items"`
+	Locale            string    `json:"locale"`
+	InternalSignature string    `json:"internal_signature"`
+	CustomerId        string    `json:"customer_id"`
+	DeliveryService   string    `json:"delivery_service"`
+	ShardKey          string    `json:"shardkey"`
+	SmId              int       `json:"sm_id"`
+	DateCreated       time.Time `json:"date_created"`
+	OofShard          string    `json:"oof_shard"`
+}
+
+
